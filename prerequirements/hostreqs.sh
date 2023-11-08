1b4efacf309867b5d91068ec9aeadcfd45ccc29d
unknown <yrezehi@gmail.com>
2023-11-07 17:26:45 -0800

    add: minor fixes

--- prerequirements/hostreqs.sh
+++ prerequirements/hostreqs.sh
@@ -6,84 +6,85 @@ done
 
 # if prompt type is sh-bash
 if [ ! -f /bin/sh ]; then
-    echo "\nWARNING: no hard link to bash"
+    echo -e "\nWARNING: no hard link to bash"
 fi
 
 # binutils less than 2.41
 if ! [ -x "$(command -v binutils)" ]; then
-    echo "\nERROR: binutils is not installed on the system!"
+    echo -e "\nERROR: binutils is not installed on the system!"
 fi
 
 # if prompt type is sh-bash
 if [ ! -f /usr/bin/yacc ]; then
-    echo "\nWARNING: no hard link to bison"
+    echo -e "\nWARNING: no hard link to bison"
 fi
 
 if ! [ -x "$(command -v diffutils)" ]; then
-    echo "\nERROR: diffutils is not installed on the system!"
+    echo -e "\nERROR: diffutils is not installed on the system!"
 fi
 
 if ! [ -x "$(command -v findutils)" ]; then
-    echo "\nERROR: findutils is not installed on the system!"
+    echo -e "\nERROR: findutils is not installed on the system!"
 fi
 
 # if prompt type is sh-bash
 if [ ! -f /usr/bin/awk ]; then
-    echo "\nWARNING: no hard link to gawk"
+    echo -e "\nWARNING: no hard link to gawk"
 fi
 
 if ! [ -x "$(command -v gcc)" ]; then
-    echo "\nERROR: GCC is not installed on the system!"
+    echo -e "\nERROR: GCC is not installed on the system!"
 fi
 
 if ! [ -x "$(command -v grep)" ]; then
-    echo "\nERROR: Grep is not installed on the system!"
+    echo -e "\nERROR: Grep is not installed on the system!"
 fi
 
 if ! [ -x "$(command -v gzip)" ]; then
-    echo "\nERROR: Gzip is not installed on the system!"
+    echo -e "\nERROR: Gzip is not installed on the system!"
 fi
 
 if [ -f /proc/version ]; then
-    echo "\nKernal: "
+    echo -e "\nKernal: "
     cat /proc/version
-    echo " has to be newer than 4.14!"   
+    echo -e " has to be newer than 4.14!"   
 fi
 
 if ! [ -x "$(command -v m4)" ]; then
-    echo "\nERROR: M4 is not installed on the system!"
+    echo -e "\nERROR: M4 is not installed on the system!"
 fi
 
 if ! [ -x "$(command -v make)" ]; then
-    echo "\nERROR: Make is not installed on the system!"
+    echo -e "\nERROR: Make is not installed on the system!"
 fi
 
 if ! [ -x "$(command -v patch)" ]; then
-    echo "\nERROR: Patch is not installed on the system!"
+    echo -e "\nERROR: Patch is not installed on the system!"
 fi
 
 if ! [ -x "$(command -v perl)" ]; then
-    echo "\nERROR: Perl is not installed on the system!"
+    echo -e "\nERROR: Perl is not installed on the system!"
 fi
 
 if ! [ -x "$(command -v python)" ]; then
-    echo "\nERROR: Python is not installed on the system!"
+    echo -e "\nERROR: Python is not installed on the system!"
 fi
 
 if ! [ -x "$(command -v sed)" ]; then
-    echo "\nERROR: Sed is not installed on the system!"
+    echo -e "\nERROR: Sed is not installed on the system!"
 fi
 
 if ! [ -x "$(command -v tar)" ]; then
-    echo "\nERROR: Tar is not installed on the system!"
+    echo -e "\nERROR: Tar is not installed on the system!"
 fi
 
 if ! [ -x "$(command -v texinfo)" ]; then
-    echo "\nERROR: Texinfo is not installed on the system!"
+    echo -e "\nERROR: Texinfo is not installed on the system!"
 fi
 
 if ! [ -x "$(command -v xz)" ]; then
-    echo "\nERROR: XZ is not installed on the system!"
+    echo -e "\nERROR: XZ is not installed on the system!"
 fi
 
+echo "Standard Report: \n"
 cat > hostreqs.sh << "EOF"
