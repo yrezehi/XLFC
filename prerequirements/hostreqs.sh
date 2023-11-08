2e9b33273f8ed91821d3d82c77ad4b44d4acd90f
unknown <yrezehi@gmail.com>
2023-11-07 17:15:43 -0800

    add: few more checks

--- prerequirements/hostreqs.sh
+++ prerequirements/hostreqs.sh
@@ -4,7 +4,7 @@ for ARGUMENT in "$@"; do
     export "${ARGUMENT%%=*}"="${ARGUMENT#*=}";
 done
 
-# 
+# if prompt type is sh-bash
 if [ ! -f /bin/sh ]; then
     echo "\nWARNING: no hard link to bash"
 fi
@@ -12,4 +12,30 @@ fi
 # binutils less than 2.41
 if ! [ -x "$(command -v binutils)" ]; then
     echo "\nERROR: binutils is not installed on the system!"
+fi
+
+# if prompt type is sh-bash
+if [ ! -f /usr/bin/yacc ]; then
+    echo "\nWARNING: no hard link to bison"
+fi
+
+if ! [ -x "$(command -v diffutils)" ]; then
+    echo "\nERROR: diffutils is not installed on the system!"
+fi
+
+if ! [ -x "$(command -v findutils)" ]; then
+    echo "\nERROR: findutils is not installed on the system!"
+fi
+
+# if prompt type is sh-bash
+if [ ! -f /usr/bin/awk ]; then
+    echo "\nWARNING: no hard link to gawk"
+fi
+
+if ! [ -x "$(command -v gcc)" ]; then
+    echo "\nERROR: GCC is not installed on the system!"
+fi
+
+if ! [ -x "$(command -v grep)" ]; then
+    echo "\nERROR: Grep is not installed on the system!"
 fi
