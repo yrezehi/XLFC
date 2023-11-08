5e4ed905b809b94a89959541528af9cb235ea636
unknown <yrezehi@gmail.com>
2023-11-07 17:24:55 -0800

    add: standard sh

--- prerequirements/hostreqs.sh
+++ prerequirements/hostreqs.sh
@@ -39,3 +39,51 @@ fi
 if ! [ -x "$(command -v grep)" ]; then
     echo "\nERROR: Grep is not installed on the system!"
 fi
+
+if ! [ -x "$(command -v gzip)" ]; then
+    echo "\nERROR: Gzip is not installed on the system!"
+fi
+
+if [ -f /proc/version ]; then
+    echo "\nKernal: "
+    cat /proc/version
+    echo " has to be newer than 4.14!"   
+fi
+
+if ! [ -x "$(command -v m4)" ]; then
+    echo "\nERROR: M4 is not installed on the system!"
+fi
+
+if ! [ -x "$(command -v make)" ]; then
+    echo "\nERROR: Make is not installed on the system!"
+fi
+
+if ! [ -x "$(command -v patch)" ]; then
+    echo "\nERROR: Patch is not installed on the system!"
+fi
+
+if ! [ -x "$(command -v perl)" ]; then
+    echo "\nERROR: Perl is not installed on the system!"
+fi
+
+if ! [ -x "$(command -v python)" ]; then
+    echo "\nERROR: Python is not installed on the system!"
+fi
+
+if ! [ -x "$(command -v sed)" ]; then
+    echo "\nERROR: Sed is not installed on the system!"
+fi
+
+if ! [ -x "$(command -v tar)" ]; then
+    echo "\nERROR: Tar is not installed on the system!"
+fi
+
+if ! [ -x "$(command -v texinfo)" ]; then
+    echo "\nERROR: Texinfo is not installed on the system!"
+fi
+
+if ! [ -x "$(command -v xz)" ]; then
+    echo "\nERROR: XZ is not installed on the system!"
+fi
+
+cat > hostreqs.sh << "EOF"
